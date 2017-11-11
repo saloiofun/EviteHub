0657041a48e5c4be173bb5b1fb8f42dc30fdf921
Sandro <saloiofun@gmail.com>
2017-11-11 11:09:06 -0800

    Update controller.js
    Deleted comments for placeholder

--- controllers/controller.js
+++ controllers/controller.js
@@ -34,17 +34,4 @@ module.exports = {
       .then(dbModel => res.json( dbModel ))
       .catch(err => res.status( 422 ).json( err ));
   }
-//   findAll: function (req, res) {
-//     db.Saved
-//       .find(req.query)
-//       .sort({ date: -1 })
-//       .then(dbModel => res.json(dbModel))
-//       .catch(err => res.status(422).json(err))
-//   },
-//   create: function (req, res) {
-//     db.Saved
-//       .create(req.body)
-//       .then(dbModel => res.json(dbModel))
-//       .catch(err => res.status(422).json(err))
-//   }
 }
