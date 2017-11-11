c78c06ea8749d382d056fe5af67bee02ab491660
Sandro Wong <saloiofun@gmail.com>
2017-11-11 15:15:55 -0800

    Add routes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const routes = require('./routes')
+const routes = require('./routes')
 const PORT = process.env.PORT || 3001
 const app = express()
 const mongoose = require('mongoose')
