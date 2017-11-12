24d2a4bf74e35f32e3d01e4e50712607138f44fc
Andres Valdovinos <avaldov1@uci.edu>
2017-11-11 22:47:11 -0800

    Added email in routes, controller still needs proper message

---
+++ controllers/emailController.js
@@ -0,0 +1,28 @@
+const db = require('../models')
+
+var nodemailer = require("nodemailer");
+var smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+      user: "[ENTER EMAIL HERE]",
+      pass: "[ENTER EMAIL PASSWORD]"
+  }
+})
+
+module.exports = {
+    deliverEmail: function(req, res) {
+      const mailOptions={
+        to : req.body.to,
+        subject : "Event Invitation",
+        text : `${req.body.user} has invited you to ${req.body.event}!\n Click on link for more details: ${req.body.url}`
+      }
+      smtpTransport.sendMail(mailOptions, function(error, response){
+       if(error){
+              console.log(error);
+       }else{
+              console.log("Email sent");
+           }
+      });
+    }
+  }
