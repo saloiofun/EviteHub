40c8af78f07835a8e058d4a1eabbd07133bebfc1
Sandro Wong <saloiofun@gmail.com>
2017-11-12 22:40:58 -0800

    Add images and cards

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,20 +1,12 @@
-// import React from 'react'
-// import Button from 'material-ui/Button'
-
-// const Dashboard = () => (
-//   <Button raised color='primary'>
-//     Dashboard
-//   </Button>
-// )
-
-// export default Dashboard
-
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
 import Paper from 'material-ui/Paper'
 import Grid from 'material-ui/Grid'
+import Button from 'material-ui/Button'
 import Typography from 'material-ui/Typography'
+import CheckboxList from '../../components/Todo'
 
 const styles = theme => ({
   root: {
@@ -27,6 +19,26 @@ const styles = theme => ({
   paper: {
     padding: 16,
     textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  card: {
+    minWidth: 275
+  },
+  media: {
+    height: 250
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+    color: theme.palette.text.secondary
+  },
+  pos: {
+    marginBottom: 12,
     color: theme.palette.text.secondary
   }
 })
@@ -47,13 +59,47 @@ function Dashboard (props) {
           <Typography type='headline'>Date</Typography>
         </Grid>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+          <CardMedia
+            className={classes.media}
+            image='/static/images/cards/progress.png'
+            title='Progress'
+          />
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        <Grid item xs={12} sm={8}>
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image='/static/images/cards/website.png'
+              title='Website'
+            />
+            <CardContent>
+              <Typography type='headline' component='h2'>
+                Title
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button dense color='primary'>
+                Share
+              </Button>
+              <Button dense color='primary'>
+                View Website
+              </Button>
+            </CardActions>
+          </Card>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        <Grid item xs={12} sm={4}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography type='headline' component='h2'>
+                To Do List
+              </Typography>
+              <CheckboxList />
+            </CardContent>
+            <CardActions align='right'>
+              <Button dense color='primary'>Share</Button>
+              <Button dense color='primary'>View All</Button>
+            </CardActions>
+          </Card>
         </Grid>
         <Grid item xs={6} sm={3}>
           <Paper className={classes.paper}>xs=6 sm=3</Paper>
