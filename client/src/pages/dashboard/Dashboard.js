5f37132295404c468b9c84a13379b89b624e759d
Sandro Wong <saloiofun@gmail.com>
2017-11-12 20:57:54 -0800

    added grid to dashboard

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,10 +1,66 @@
+// import React from 'react'
+// import Button from 'material-ui/Button'
+
+// const Dashboard = () => (
+//   <Button raised color='primary'>
+//     Dashboard
+//   </Button>
+// )
+
+// export default Dashboard
+
 import React from 'react'
-import Button from 'material-ui/Button'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Paper from 'material-ui/Paper'
+import Grid from 'material-ui/Grid'
 
-const Dashboard = () => (
-  <Button raised color='primary'>
-    Dashboard
-  </Button>
-)
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+})
 
-export default Dashboard
+function Dashboard (props) {
+  const { classes } = props
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Dashboard)
