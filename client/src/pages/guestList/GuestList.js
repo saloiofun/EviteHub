b3f13059130d4802f8d34856652987830ca9d7c2
CODING <CODING>
2017-11-19 00:04:46 -0800

    update guestlist

--- client/src/pages/guestList/GuestList.js
+++ client/src/pages/guestList/GuestList.js
@@ -1,5 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import API from '../../utils/Api'
+import Switch from 'material-ui/Switch'
+import IconButton from 'material-ui/IconButton'
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table'
+import Edit from 'material-ui-icons/Edit'
+import Delete from 'material-ui-icons/Delete'
+import Chip from 'material-ui/Chip'
+import AddIcon from 'material-ui-icons/Add'
+import FaceIcon from 'material-ui-icons/Face'
+import Avatar from 'material-ui/Avatar'
+import TextField from 'material-ui/TextField'
+import Dialog, { DialogActions, DialogContent, DialogContentText, DialogTitle } from 'material-ui/Dialog'
 import Button from 'material-ui/Button'
+import Tooltip from 'material-ui/Tooltip'
+import { withStyles } from 'material-ui/styles'
+import PropTypes from 'prop-types'
 
 const GuestList = () => (
   <Button raised color='primary'>
