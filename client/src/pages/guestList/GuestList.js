1aab4a5459c105538e8c208f8bf0c77f430f7938
Sandro Wong <saloiofun@gmail.com>
2017-11-19 14:09:38 -0800

    Cleaned semicolon

--- client/src/pages/guestList/GuestList.js
+++ client/src/pages/guestList/GuestList.js
@@ -56,26 +56,31 @@ class GuestList extends Component {
     API.getGuests()
       .then(res => this.setState({ allGuest: res.data }))
       .catch(err => console.log(err))
-  };
+  }
+
   // handle delete a guest with the given id
   deleteGuest = id => {
     API.deleteGuest(id)
       .then(res => this.loadGuest())
       .catch(err => console.log(err))
-  };
+  }
+
   // open new guest modal
   newDialogOpen = () => {
     this.setState({ newDialog: true })
-  };
+  }
+
   // close new guest modal
   newDialogClose = () => {
     this.setState({ newDialog: false })
-  };
+  }
+
   // handle input changes
   handleInputChange = event => {
     const { name, value } = event.target
     this.setState({ [name]: value })
-  };
+  }
+
   // Handle saves a guest to the database then close modal and call all guest
   saveGuest = () => {
     if (this.state.name && this.state.party && this.state.email) {
@@ -83,13 +88,15 @@ class GuestList extends Component {
         .then(res => this.loadGuest(), this.newDialogClose())
         .catch(err => console.log(err))
     }
-  };
+  }
+
   // handle RSVP toggle
   rsvpToggle = (id, rsvp) => (event, checked) => {
     API.updateGuest(id, {'rsvp': !rsvp})
     .then(res => this.loadGuest())
       .catch(err => console.log(err))
-  };
+  }
+
   render () {
     const { classes } = this.props
     return (
@@ -102,7 +109,9 @@ class GuestList extends Component {
             </Avatar>
          }
           label=' Add a Guest ' style={{backgroundColor: '#009688', color: 'white'}}
-          onRequestDelete={this.newDialogOpen} deleteIcon={<AddIcon style={{color: 'white'}} />}
+          onClick={this.newDialogOpen}
+          onRequestDelete={this.newDialogOpen}
+          deleteIcon={<AddIcon style={{color: 'white'}} />}
        />
         <Table>
           <TableHead>
