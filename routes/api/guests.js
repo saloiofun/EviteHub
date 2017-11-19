d7c6b2a2042902eb643f8b6d39a8b09b0f608f93
CODING <CODING>
2017-11-18 23:51:10 -0800

    update api

--- routes/api/guests.js
+++ routes/api/guests.js
@@ -7,8 +7,8 @@ router.route('/')
   .post(controller.createGuest)
 
 // Matches with "/api/guests/:id"
-router
-  .route('/:id')
+router.route('/:id')
+  .get(controller.findGuestById)
   .put(controller.updateGuest)
   .delete(controller.removeGuest)
 
