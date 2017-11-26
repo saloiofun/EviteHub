86d5874eeac79c43283f561d05a0147c6ab74f94
mariomelchor <mario@mariomelchor.com>
2017-11-26 00:35:18 -0800

    Changed dashboard component to a state component, getting user profile when authenticated, passing profle props to PageHeader component

--- client/src/components/PageHeader.js
+++ client/src/components/PageHeader.js
@@ -1,29 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
-import Paper from 'material-ui/Paper'
 import Typography from 'material-ui/Typography'
+import Avatar from 'material-ui/Avatar'
+import Card, { CardHeader } from 'material-ui/Card'
 
 const styles = theme => ({
-  root: theme.mixins.gutters({
-    paddingTop: 16,
-    paddingBottom: 16,
+  card: {
     marginBottom: theme.spacing.unit * 3
-  })
+  },
+  bigAvatar: {
+    width: 75,
+    height: 75
+  }
 })
 
 function PaperSheet (props) {
-  const { classes } = props
+  const { classes, profile } = props
   return (
     <div>
-      <Paper className={classes.root} elevation={4}>
-        <Typography type='display1' component='h3'>
-          {props.title}
-        </Typography>
-        <Typography type='body1' component='p'>
-          {props.body}
-        </Typography>
-      </Paper>
+      <Typography type='display1' component='h3'>
+        {props.title}
+      </Typography>
+      <Typography type='body1' component='p'>
+        {props.body}
+      </Typography>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={<Avatar alt={profile.name} src={profile.picture} className={classes.bigAvatar} />}
+          title={profile.name}
+          subheader={`UserID: ${profile.sub}`}
+        />
+      </Card>
     </div>
   )
 }
