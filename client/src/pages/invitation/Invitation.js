5599520712fcd5ff937eaf111cd42681181d8d99
CODING <CODING>
2017-11-28 20:58:27 -0800

    add invitation

--- client/src/pages/invitation/Invitation.js
+++ client/src/pages/invitation/Invitation.js
@@ -14,3 +14,42 @@ import jsPDF from 'jspdf'
 import Input, { InputLabel } from 'material-ui/Input'
 import {FormControl} from 'material-ui/Form'
 import MenuItem from 'material-ui/Menu/MenuItem'
+
+const styles = theme => ({
+  root: {
+    flex: '1 1 100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '80%'
+    },
+    margin: '0 auto'
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: '#009688'
+  },
+  title: { margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`, color: '#009688' },
+  Avatar: { backgroundColor: '#009688' },
+  icon: { color: 'white' }
+})
+
+const bg = [
+  {
+    value: 'url("/static/images/invitation/paper01.jpg")',
+    label: 'paper01'
+  },
+  {
+    value: 'url("/static/images/invitation/paper02.jpg")',
+    label: 'paper02'
+  },
+  {
+    value: 'url("/static/images/invitation/paper03.jpg")',
+    label: 'paper03'
+  }
+]
+
+const font = [
+    { value: 'Arial' },
+    { value: 'Times New Roman' },
+    { value: 'Trebuchet MS' }
+]
