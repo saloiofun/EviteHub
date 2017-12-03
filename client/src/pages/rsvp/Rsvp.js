7f99405ea9ad5145219a20c756a2c39a60f0e58d
Andres Valdovinos <valdovinosandres@ymail.com>
2017-12-03 12:23:25 -0800

    Added name and party input fields, needs styling

--- client/src/pages/rsvp/Rsvp.js
+++ client/src/pages/rsvp/Rsvp.js
@@ -6,9 +6,9 @@ import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import Card, { CardContent } from 'material-ui/Card'
 import moment from 'moment'
-import { FormControl, FormControlLabel } from 'material-ui/Form'
-import Radio, { RadioGroup } from 'material-ui/Radio'
+import Radio from 'material-ui/Radio'
 import Grid from 'material-ui/Grid'
+import TextField from 'material-ui/TextField'
 import Button from 'material-ui/Button'
 
 const styles = theme => ({
@@ -17,7 +17,7 @@ const styles = theme => ({
     display: 'flex',
     flexWrap: 'wrap',
     minWidth: 300,
-    maxHeight: 400,
+    // maxHeight: 400,
     paddingTop: 16,
     paddingBottom: 16,
     width: '50%',
@@ -42,7 +42,9 @@ class Rsvp extends React.Component {
       location: 'Irvine, CA',
       Date: 'March 8, 2018',
       Time: '3:15 PM',
-      rsvp: ''
+      rsvp: '',
+      name: '',
+      party: 0
     }
   }
   componentDidMount () {
@@ -55,6 +57,7 @@ class Rsvp extends React.Component {
     API.getGuestByHash(hash)
     .then((data) => {
       console.log('Guest:', data)
+      this.setState({ guestObject: data })
     })
     .catch((err) => {
       throw (err)
@@ -68,9 +71,44 @@ class Rsvp extends React.Component {
     })
   }
 
+  onSubmit = () => {
+    switch (this.state.rsvp) {
+      case 'Accept':
+        API.updateGuest(this.state.guestObject._id,
+          {
+            guestName: this.state.name,
+            guestParty: this.state.party,
+            rsvp: true
+          })
+        .then(data => {
+          console.log(data)
+        })
+        break
+      case 'Reject':
+        API.updateGuest(this.state.guestObject._id,
+          {
+            guestName: this.state.guestName,
+            guestParty: this.state.guestParty,
+            rsvp: false
+          })
+        .then(data => {
+          console.log(data)
+        })
+        break
+      default:
+        console.log('Error did not select radio button')
+    }
+  }
+
   handleRSVP = (event, rsvp) => {
     console.log(rsvp)
     this.setState({ rsvp })
+  }
+
+    // handle input changes
+  handleInputChange = event => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
   }
 
   render () {
@@ -78,43 +116,75 @@ class Rsvp extends React.Component {
 
     return (
       <Paper className={classes.root} elevation={8}>
-        <Grid container spacing={12} justify='center'>
-          <Grid item xs={12} sm={10}>
-            <Card>
-              {/* <CardMedia
+        <Grid container spacing={12}>
+          <Grid item xs={12}>
+            <Grid container justify='center'>
+              <Grid item xs={12} sm={10}>
+                <Card>
+                  {/* <CardMedia
                 className={classes.media}
                 image='/static/images/events/event.jpg'
                 title='Event'
               /> */}
-              <CardContent>
-                <Typography type='headline' component='h2'>
-                  {this.state.event.eventName}
-                </Typography>
-                <Typography className={classes.info} component='p'>
+                  <CardContent>
+                    <Typography type='headline' component='h2'>
+                      {this.state.event.eventName}
+                    </Typography>
+                    <Typography className={classes.info} component='p'>
                     Location: {this.state.event.location} <br />
                     Date: {moment(this.state.event.date).format('MMMM Do YYYY')} <br />
                     Time: {moment(this.state.event.time).format('hh:mm A')}
-                </Typography>
-                <Typography component='p'>
+                    </Typography>
+                    <Typography component='p'>
                     Description: {this.state.event.description}
-                </Typography>
-              </CardContent>
-            </Card>
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              margin='dense'
+              name='name'
+              label='Guest Name'
+              value={this.state.name}
+              onChange={this.handleInputChange}
+          />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              margin='dense'
+              type='number'
+              name='party'
+              inputProps={{min: 0}}
+              label='No. of Party'
+              value={this.state.party}
+              onChange={this.handleInputChange}
+          />
           </Grid>
           <Grid item xs={12} sm={4}>
             <Typography type='h1'> Will You Be Attending? </Typography>
-            <FormControl component='fieldset' required className={classes.formControl}>
-              <RadioGroup
-                aria-label='rsvp'
-                name='rsvp'
-                className={classes.group}
-                value={this.state.rsvp}
-                onChange={this.handleRSVP}
-              >
-                <FormControlLabel value='Accept' control={<Radio />} label='Accept' />
-                <FormControlLabel value='Reject' control={<Radio />} label='Reject' />
-              </RadioGroup>
-            </FormControl>
+            <span className={{verticalAlign: 'middle'}}>
+              <Radio
+                checked={this.state.selectedValue === 'Accept'}
+                onChange={this.handleChange}
+                value='Accept'
+                name='Accept'
+                aria-label='Accept'
+              />
+               Accept
+            </span>
+            <span verticalAlign='middle'>
+              <Radio
+                checked={this.state.selectedValue === 'Reject'}
+                onChange={this.handleChange}
+                value='Reject'
+                name='Reject'
+                aria-label='Reject'
+              />
+              Reject
+              </span>
           </Grid>
         </Grid>
         <Button className={classes.button} onClick={this.onSubmit} color='primary'>
