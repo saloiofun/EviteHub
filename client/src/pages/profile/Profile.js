2c6902e327f8510f4aab9c935fda99c46e971800
mariomelchor <mario@mariomelchor.com>
2017-12-07 18:52:31 -0800

    Using Redux for profile

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
-import AuthAPI from '../../utils/AuthAPI'
 import PageHeader from '../../components/PageHeader'
 import Card, {CardContent } from 'material-ui/Card'
 import List, { ListItem, ListItemText } from 'material-ui/List'
@@ -10,6 +9,8 @@ import Mail from 'material-ui-icons/Mail'
 import Avatar from 'material-ui/Avatar'
 import teal from 'material-ui/colors/teal'
 import Grid from 'material-ui/Grid'
+import compose from 'recompose/compose'
+import { connect } from 'react-redux'
 
 const styles = theme => ({
   root: {
@@ -24,10 +25,6 @@ const styles = theme => ({
       paddingRight: theme.spacing.unit * 3
     }
   },
-  title: {
-    marginBottom: 16,
-    fontSize: 14
-  },
   profileImage: {
     width: '100%'
   },
@@ -39,52 +36,8 @@ const styles = theme => ({
 })
 
 class Profile extends Component {
-  componentWillMount () {
-    this.setState({ profile: {} })
-    const { userProfile, getProfile } = this.props.auth
-    if (!userProfile) {
-      getProfile((err, profile) => {
-        this.setState({ profile })
-      })
-    } else {
-      this.setState({ profile: userProfile })
-    }
-  }
-
-  componentDidMount () {
-    // Get Token
-    AuthAPI.getToken()
-    .then(res => {
-      // Use Token and call API to get Users
-      AuthAPI.getUsers(res.data.token_type, res.data.access_token)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
-
-      // Use Token and call API to update User meta
-      AuthAPI.updateUser(res.data.token_type, res.data.access_token, this.state.profile.sub)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
-
-      // Use Token and call API to get User by Email
-      AuthAPI.getUserByEmail(res.data.token_type, res.data.access_token, this.state.profile.email)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
-
-      // Use Token and call API to get Events by Email
-      AuthAPI.getEventsByEmail(res.data.token_type, res.data.access_token, this.state.profile.email)
-      .then(res => console.log(res.data[0]))
-      .catch(err => console.log(err))
-
-      // Use Token and call API to get User by ID
-      AuthAPI.getUserByID(res.data.token_type, res.data.access_token, this.state.profile.sub)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
-    })
-  }
-
   render () {
-    const { profile } = this.state
-    const { classes } = this.props
+    const { classes, auth } = this.props
 
     return (
       <div className={classes.root}>
@@ -95,26 +48,26 @@ class Profile extends Component {
               <Grid container spacing={24}>
 
                 <Grid item xs={12} sm={1}>
-                  <img src={profile.picture} alt='profile' className={classes.profileImage} />
+                  <img src={auth.profile.picture} alt='profile' className={classes.profileImage} />
                 </Grid>
 
                 <Grid item xs={12} sm={11}>
                   <List>
                     <ListItem divider>
-                      <ListItemText primary={(<h1>{profile.name}</h1>)} />
+                      <ListItemText primary={auth.profile.name} />
                     </ListItem>
                     <ListItem divider>
                       <Avatar className={classes.iconAvatar}>
                         <AccountCircle />
                       </Avatar>
-                      <ListItemText primary={profile.sub} secondary='User ID' />
+                      <ListItemText primary={auth.profile.sub} secondary='User ID' />
                     </ListItem>
-                    { (profile.email) &&
+                    { (auth.profile.email) &&
                       <ListItem divider>
                         <Avatar className={classes.iconAvatar}>
                           <Mail />
                         </Avatar>
-                        <ListItemText primary={profile.email} secondary='Email' />
+                        <ListItemText primary={auth.profile.email} secondary='Email' />
                       </ListItem>
                     }
                   </List>
@@ -132,4 +85,14 @@ Profile.propTypes = {
   classes: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(Profile)
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default compose(
+  withStyles(styles, {
+    name: 'Profile'
+  }), connect(mapStateToProps)
+)(Profile)
